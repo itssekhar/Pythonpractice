42ecb93aedcf78cfacef3d7b0b936e58ae82e8b1
itssekhar <itssekhar511@gmail.com>
2019-09-22 18:58:11 -0500

    oddandeven program

--- oddandeven.py
+++ oddandeven.py
@@ -5,4 +5,15 @@ if(res>0):
     print("odd number")
 else:
     print("even number")
-        
+
+ """ Using function"""    
+
+ def Number(n):
+    res=n%2
+    if res>0:
+        return "odd" 
+    else:
+        return "even"
+    
+Number(2)
+       
