5edb0381b2ee030589bcbddbe15a17c4aadb123c
itssekhar <itssekhar511@gmail.com>
2019-09-24 00:51:07 -0500

    findanumberfromlist

--- Find.py
+++ Find.py
@@ -2,7 +2,6 @@ def find1(ordered_list,ele_find):
     for ele in ordered_list:
         if ele == ele_find:
             return True
-        
     return False
 if __name__ == "__main__":
     l = [2,4,6,7]
