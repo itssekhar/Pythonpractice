6d8add97b2277d81afbf55b5ac72ca581da1aa37
itssekhar <itssekhar511@gmail.com>
2019-12-02 12:18:57 -0600

    coinscount

--- coins.py
+++ coins.py
@@ -1,3 +1,4 @@
+#Num of coins
 def num_coins(cents):
     coins =[25,10,5,1]
     num = 0
@@ -10,4 +11,19 @@ def num_coins(cents):
     return num
 
 print(num_coins(26))
-           
+
+#Num of min coins
+def num_coins(cents):
+    coins = [25,10,5,1]
+    num = 0
+    for i in range(0,len(coins)):
+        for j in range(1,len(coins)):
+            if coins[i]>coins[j]:
+                coins1 = coins[i]
+                while cents>=coins1:
+                    print(coins1)
+                    cents = cents-coins1
+                    num = num + 1
+    return num
+
+print(num_coins(100))
