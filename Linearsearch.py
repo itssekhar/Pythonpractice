8a8781fae7bd6d7470c22472b5583c192d307457
itssekhar <itssekhar511@gmail.com>
2019-12-23 09:49:07 -0800

    linearsearch

--- Linearsearch.py
+++ Linearsearch.py
@@ -9,3 +9,14 @@ if __name__ == "__main__":
     data = [2,3,4,5,6]
     print(linear(data,6))
     
+
+
+def linear(data,target):
+    for i in range(0,len(data)):
+        if data[i]==target:
+            return True
+    return False
+if __name__ == "__main__":
+    data = [2,3,4,5,9]
+    print(linear(data,6))
+    
